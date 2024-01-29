3b71b02362e2dbd295eae8581caf5cf4d93d3135
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2024-01-29 22:51:08 +0100

    feat: landing page upper section done

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
-    selector: 'app-landing-page',
-    standalone: true,
-    templateUrl: './landing-page.component.html',
-    styleUrl: './landing-page.component.css',
-    imports: [NavbarComponent]
+  selector: 'app-landing-page',
+  standalone: true,
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css',
+  imports: [NavbarComponent, FontAwesomeModule],
 })
 export class LandingPageComponent {
-
+  faCoffee = faCoffee;
+  faArrowRight = faArrowRight;
 }
