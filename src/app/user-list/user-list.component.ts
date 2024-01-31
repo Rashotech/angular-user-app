588ba7d1e626992c1bd6c3c23968d04567a08258
Rasheed Ayoade <rashotech@gmail.com>
2024-01-31 17:36:01 +0100

    bug fix

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,40 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from '../../data/user.interface';
-import { CardComponent } from "../card/card.component";
+import { CardComponent } from '../card/card.component';
 import { CardplaceholderComponent } from '../cardplaceholder/cardplaceholder.component';
 import { UserService } from '../user.service';
 import { HttpClientModule } from '@angular/common/http';
-import { SearchBoxComponent } from "../search-box/search-box.component";
+import { SearchBoxComponent } from '../search-box/search-box.component';
 
 @Component({
-    selector: 'app-user-list',
-    standalone: true,
-    templateUrl: './user-list.component.html',
-    styleUrl: './user-list.component.css',
-    imports: [CardComponent, HttpClientModule, CardplaceholderComponent]
+  selector: 'app-user-list',
+  standalone: true,
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.css',
+  imports: [
+    CardComponent,
+    HttpClientModule,
+    CardplaceholderComponent,
+    SearchBoxComponent,
+  ],
 })
 export class UserListComponent implements OnInit {
-    users: IUser[] = [];
-    loading: boolean = false;
-    searchTerm = "";
+  users: IUser[] = [];
+  loading: boolean = false;
+  searchTerm = '';
 
-    constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
-    filterUsers() {
-        return this.users.filter(user => user.name.toLowerCase().includes(this.searchTerm.toLowerCase()))
-    }
+  filterUsers() {
+    return this.users.filter((user) =>
+      user.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 
-    ngOnInit() {
-        this.loading = true;
-        this.userService.getUsers().subscribe({
-            next: data => {
-                this.users = data;
-                this.loading = false;
-            },
-            error: error => {
-                console.log(error)
-            },
-            complete: () => console.info("Data was successfully retrieved")
-        });
-    };
+  ngOnInit() {
+    this.loading = true;
+    this.userService.getUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => console.info('Data was successfully retrieved'),
+    });
+  }
 }
