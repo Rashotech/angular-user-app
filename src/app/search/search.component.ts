6f82f3d84018ca4d406d54a215532f981dcaed30
Rasheed Ayoade <rashotech@gmail.com>
2024-01-31 23:22:20 +0100

    fix: search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,24 +1,25 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Route } from '@angular/router';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSliders } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  
-  
   faf = faSliders;
   
-  constructor() {}
+  @Input({ required: true})
+  searchTerm!: string;
 
-  doSearch() {
-    
-  }  
+  @Output()
+  searchTermChange: EventEmitter<string> = new EventEmitter();
+
+  handleChange(value: string) {
+    this.searchTermChange.emit(value);
+  }
 }
