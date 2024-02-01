c543275e0801a1975637d3e5aabe62dd25217bbd
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2024-02-01 03:56:52 +0100

    feat: details page completed with loading skeleton

---
+++ src/app/details-skeleton/details-skeleton.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-skeleton',
+  standalone: true,
+  imports: [],
+  templateUrl: './details-skeleton.component.html',
+  styleUrl: './details-skeleton.component.css'
+})
+export class DetailsSkeletonComponent {
+
+}
--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -3,38 +3,41 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { IUser } from '../../data/user.interface';
 import { UserService } from '../user.service';
-import { CardComponent } from "../card/card.component";
+import { DetailsSkeletonComponent } from '../details-skeleton/details-skeleton.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-    selector: 'app-user-detail',
-    standalone: true,
-    templateUrl: './user-detail.component.html',
-    styleUrl: './user-detail.component.css',
-    imports: [HttpClientModule, RouterOutlet, CardComponent]
+  selector: 'app-user-detail',
+  standalone: true,
+  templateUrl: './user-detail.component.html',
+  styleUrl: './user-detail.component.css',
+  imports: [HttpClientModule, RouterOutlet, FontAwesomeModule, DetailsSkeletonComponent],
 })
-
 export class UserDetailComponent implements OnInit {
-    user: IUser | null = null;
-    loading: boolean = false;
-    @Input('id') userId = '';
+  user: IUser | null = null;
+  loading: boolean = false;
+  faLink = faLink;
+  @Input('id') userId = '';
 
-    constructor(
-        private readonly userService: UserService,
-        private router: Router
-    ) { }
+  constructor(
+    private readonly userService: UserService,
+    private router: Router
+  ) {}
 
-    ngOnInit() {
-        this.loading = true;
-        this.userService.getSingleUser(Number(this.userId)).subscribe({
-            next: data => {
-                this.user = data;
-                this.loading = false;
-            },
-            error: error => {
-                console.log(error);
-                return this.router.navigate(['/404'], { skipLocationChange: true });
-            },
-            complete: () => console.info("Data was successfully retrieved")
-        });
-    };
+  ngOnInit() {
+    this.loading = true;
+    this.userService.getSingleUser(Number(this.userId)).subscribe({
+      next: (data) => {
+            this.user = data;
+            console.log(this.user);
+        this.loading = false;
+      },
+      error: (error) => {
+        console.log(error);
+        return this.router.navigate(['/404'], { skipLocationChange: true });
+      },
+      complete: () => console.info('Data was successfully retrieved'),
+    });
+  }
 }
